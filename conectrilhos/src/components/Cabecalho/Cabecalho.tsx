4a659fb323f476dfa2993a5a083d9ae3cc69c9cd
gaaaabz <gaby8gomes@gmail.com>
2025-03-18 14:05:46 -0300

    cabecalho estilizado

--- conectrilhos/src/components/Cabecalho/Cabecalho.tsx
+++ conectrilhos/src/components/Cabecalho/Cabecalho.tsx
@@ -4,7 +4,7 @@ import Menu from "../Menu/Menu";
 export default function cabecalho() {
     return (
         <header className="cabecalho">
-        <Image src="/CCR-Logo-01.png" alt="Logo da ccr" width={40} height={40} />
+        <Image src="/CCR-Logo-01.png" alt="Logo da ccr" width={90} height={50} />
         <Menu />
         </header>
     );
