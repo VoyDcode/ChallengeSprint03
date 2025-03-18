fac4012c47f7926e1aac11d61afde46f06c36181
gaaaabz <gaby8gomes@gmail.com>
2025-03-18 14:47:32 -0300

    imagem de fundo adicionada

--- conectrilhos/src/app/layout.tsx
+++ conectrilhos/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Cabecalho from "@/components/Cabecalho/Cabecalho";
+import background from "/public/background-img-02.jpg";
 
 export const metadata: Metadata = {
   title: "Conectrilhos",
