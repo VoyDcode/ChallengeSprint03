5a6f20576ae7e3dfeaafa83a78313582df5efbe9
Natasha-Mauricio <natashamssant@gmail.com>
2025-03-19 20:35:36 -0300

    Por favor arrumar os paths pós merge

--- conectrilhos/src/components/Menu/Menu.tsx
+++ conectrilhos/src/components/Menu/Menu.tsx
@@ -4,8 +4,8 @@ export default function Menu() {
     return (
      <nav>
         <ul>
-            <li className="inline p-10"><Link href="/">Home</Link></li>
-            <li className="inline p-10"><Link href="/rota">Rota</Link></li>
+            <li className="inline p-10"><Link href="/home">Home</Link></li>
+            <li className="inline p-10"><Link href="">Rota</Link></li>
             <li className="inline p-10"><Link href="/page">Page</Link></li>
         </ul>
      </nav>
